3ae9b8469260a69644c8f70bfd776307eb5d294b
Bananaz21 <72627687+cooki-chan@users.noreply.github.com>
2022-12-08 17:04:04 -0500

    day8 after so much work :D

--- day8/challenge2.py
+++ day8/challenge2.py
@@ -1,5 +1,5 @@
 input = ""
-with open('day8/exampleInp.txt') as i:
+with open('day8/inp1.txt') as i:
     input = i.readlines()
     for i in range(0, len(input)-1):
         input[i] = input[i].replace("\n", "")
@@ -7,20 +7,51 @@ with open('day8/exampleInp.txt') as i:
 def checkTreeScore(x:int, y:int):
     treeHeight = int(input[y][x])
 
-    right = 0
-    left = 0
+    right = -1
+    left = -1
     for i in range(len(input[y])):
-        checkHeight = input[y][i]
+        checkHeight = int(input[y][i])
         if checkHeight >= treeHeight:
-            if i > x and right == 0: #ON RIGHT OF X, find first instance
-                right = i
+            if i > x and right == -1: #ON RIGHT OF X, find first instance
+                right = i - x
             elif i < x: #ON LEFT OF X, find last instance
-                left = i
+                left = x - i
+            #print(f"{i}, {x}")
     
-    if right == 0 and y != len(input[y])-1:
-        right = len(input[y])-1 - x
-    if left == 0 and y != 0:
-        left = x + 1
+    if right == -1:
+        if x != len(input[y])-1:
+            right = len(input[y])-1 - x
+        else:
+            right = 0
+    if left == -1 :
+        if x != 0:
+            left = x
+        else:
+            left = 0
+
+    top = -1
+    bot = -1
+    for i in range(len(input)):
+        checkHeight = int(input[i][x])
+        if checkHeight >= treeHeight:
+            if i > y and bot == -1: #ON BOT OF X, find first instance
+                bot = i - y
+            elif i < y: #ON LTOP OF X, find last instance
+                top = y - i
+            #print(f"{i}, {x}")
+    
+    if bot == -1 :
+        if y != len(input[y])-1:
+            bot = len(input[y]) - 1 - y
+        else:
+            bot = 0
+    if top == -1 :
+        if y != 0:
+            top = y
+        else:
+            top = 0
+
+    return [right, left, bot, top]
 
 highestTreeScore = 0
 for y in range(len(input)):
@@ -29,6 +60,6 @@ for y in range(len(input)):
         treeScore = treeScoreList[0] * treeScoreList[1] * treeScoreList[2] * treeScoreList[3]
         if treeScore > highestTreeScore:
             highestTreeScore = treeScore
-        print(f"{str(x)}, {str(y)} = {input[y][x]}| {treeScoreList[0]} * {treeScoreList[1]} * {treeScoreList[2]} * {treeScoreList[3]} = {str(treeScore)}")
+        print(f"{str(x)}, {str(y)} = {input[y][x]}| {treeScoreList[0]} * {treeScoreList[1]} * {treeScoreList[2]} * {treeScoreList[3]}= {str(treeScore)}")
 
 print(highestTreeScore)
