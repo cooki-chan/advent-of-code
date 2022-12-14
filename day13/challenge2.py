b8f472134306a668fdad6a6440f4b5acdbd6e945
Ethan Nguyen <72627687+cooki-chan@users.noreply.github.com>
2022-12-14 09:02:42 -0500

    ...it works, give it a min or 10

--- day13/challenge2.py
+++ day13/challenge2.py
@@ -43,9 +43,34 @@ def recursiveCheck(left, right):
         return 0
     return 1
 
-out = 0
-for i in range(len(input)):
-    if recursiveCheck(json.loads(input[i*3]), json.loads(input[i*3+1])) == 1:
-        out+=i+1
+validPak = []
+for i in range(len(input)//3 + 1):
+    validPak.append(json.loads(input[i*3]))
+    validPak.append(json.loads(input[i*3+1]))
+validPak.append([[2]])
+validPak.append([[6]])
 
-print(out)
+sorted = False
+while not sorted:
+    sorted = True
+    for i in range(len(validPak)-1):
+        if not recursiveCheck(validPak[i], validPak[i+1]) == 1:
+            sorted = False
+            temp = validPak[i+1]
+            validPak[i+1] = validPak[i]
+            validPak[i] = temp
+        else:
+            print("pass")
+
+div2 = 0
+div6 = 0
+for i in range(len(validPak)):
+    if validPak[i] == [[2]]:
+        div2 = i+1
+    if validPak[i] == [[6]]:
+        div6 = i+1
+    print(validPak[i])
+
+print(div2)
+print(div6)
+print(div2 * div6)
