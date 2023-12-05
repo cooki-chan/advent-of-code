7634705c67336952210490c32ab58365119cf305
Ethan Nguyen <ethan41717@gmail.com>
2023-12-05 11:07:38 -0500

    yo i like dont know what im doing

--- day5/part2.py
+++ day5/part2.py
@@ -1,58 +1,83 @@
-input = open('day5/inp.txt').readlines()
+input = open('day5/inp2.txt').readlines()
 
 rawseeds = input[0].split("seeds: ")[1].split("  ")
-outputs = []
+seeds = []
 
-
-
+for i in rawseeds:
+    temp = i.split(" ")
+    seeds.append((temp[0], str(int(temp[0]) + int(temp[1]))))
 #idea: save seeds as range from first to last
 #use range math to apply the things, and just create new tuples when you compare to a new range. 
 #from there, you can find the lowest numbers within the ranges when done
 
-print("auojihbgnsoueilj")
-for i in rawseeds:
-    seeds = []
-    for j in range(int(i.split(" ")[1])):
-        seeds.append(int(i.split(" ")[0])+j)
-
-    currSection = {}
-    recording = False
-    for i, content in enumerate(input):
-        if "\n" in content:
-            content = content[:-1]
-        
-        if content == "":
-            recording = False
-            for j, value in enumerate(seeds):
-                for k in currSection.keys():
-                    print(j)
-                    if int(value) >= int(k) and int(value) <= int(k) + int(currSection[k][1]):
-                        modifier = int(currSection[k][0]) - int(k)
-                        print(f"{value} + {modifier}")
-                        seeds[j] = str(int(value) + int(currSection[k][0]) - int(k))
-                        break
-            
-            currSection = {}
-
-        if recording:
-            content = content.split(" ")
-            currSection[content[1]] = (content[0], content[2])
-
-        if "map:" in content:
-            recording = True
+# input = (output, range)
+currSection = {}
+recording = False
+for i, content in enumerate(input):
+    if "\n" in content:
+        content = content[:-1]
     
-    lowest  = int(seeds[0])
-    for i in seeds:
-        if int(i) < lowest:
-            lowest = int(i)
-    print(lowest)
-    outputs.append(lowest)
+    #if encounter \n, do the math b/c end section
+    if content == "":
+        recording = False
+        newSeeds = []
+        print(str(currSection))
+
+        for j, value in enumerate(seeds):
+            outSeed = []
+            for k in currSection.keys():
+                if int(k) < int(value[1]) and int(k) >= int(value[0]) and int(value[1]) > int(k) + int(currSection[k][1]): #check if left bound of seed range fits but right doesnt
+                    newSeedStart = str(int(value[0]) + int(currSection[k][0]) - int(k)) #range applies to beginning
+                    newSeedEnd = str(int(currSection[k][0]))
+                    outSeed.append((newSeedStart, newSeedEnd))
+
+                    newSeedStart = str(k) #range doesnt apply to end
+                    newSeedEnd = str(int(value[1]))
+                    outSeed.append((newSeedStart, newSeedEnd))
+
+                if int(value[0]) < int(k) and int(value[1]) >= int(k) and int(value[1]) < int(k) + int(currSection[k][1]): #check if right bound fits but left doesnt
+                    newSeedStart = str(int(value[0])) #range doent apply to beginning
+                    newSeedEnd = str(int(k)-1)
+                    newSeeds.append((newSeedStart, newSeedEnd))
+
+                    newSeedStart = str(currSection[k][0]) #range applies to end
+                    newSeedEnd = str(int(currSection[k][0]) + int(currSection[k][0]) - int(k))
+                    newSeeds.append((newSeedStart, newSeedEnd))
+                
+                if int(value[0]) > int(k) and int(value[1]) < int(k) + int(currSection[k][1]):
+                    newSeedStart = str(int(value[0]) + int(currSection[k][0]) - int(k)) #range applies to end
+                    newSeedEnd = str(int(value[1]) + int(currSection[k][0]) - int(k))
+                    newSeeds.append((newSeedStart, newSeedEnd))
+
+            if outSeed == []:
+                newSeeds.append(value)
+            else:
+                for k in outSeed:
+                    newSeeds.append(k)
+                
+                
+        seeds = newSeeds
+        print(str(seeds) + "\n")
 
 
 
-lowest  = int(outputs[0])
-for i in outputs:
-    print(i)
-    if int(i) < lowest:
-        lowest = int(i)
+        
+        currSection = {}
+
+    #record info
+    if recording:
+        content = content.split(" ")
+        currSection[content[1]] = (content[0], content[2])
+
+    #start recording
+    if "map:" in content:
+        recording = True
+
+
+
+
+lowest  = int(seeds[0][0])
+for i in seeds:
+    if int(i[0]) < lowest:
+        lowest = int(i[0])
 print(lowest)
